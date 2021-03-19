8861221356230d1cccee2812382aa8d9030d4dcc
Shenghui Li <shenghui.li@it.uu.se>
2021-03-19 01:37:00 +0100

    Update

--- experiments/shakespeare.sh
+++ experiments/shakespeare.sh
@@ -23,25 +23,21 @@ pushd models/ || exit
 method=$1
 
 
-for seed in 0 1 2 3 4
+for attack_type in 3
+  do
+    for pc in 0.3
     do
-        output_name=${dataset}_${method}_${pc}_${ps}_${attack_type}_${lambda}_${seed}
-        python main.py --dataset ${dataset} --model ${model} --setup_clients ${setup_clients} -lr ${lr} --batch-size ${batch_size} --num_actors ${actors} --num_gpus ${gpus} --seed ${seed}  --clients-per-round ${per_round} --lamb ${lambda} --num-epochs ${epochs} --num-rounds ${num_rounds} --method ${method} -pc ${pc} -ps ${ps} --attack_type ${attack_type} --metrics-dir ../experiments/results/${dataset} --metrics_name \
-          ${output_name} > ../experiments/log/${dataset}/${output_name}.txt
+      for seed in 0
+          do
+            output_name=${dataset}_$1_${pc}_${ps}_${attack_type}_${lambda}_${seed}
+            python main.py --dataset ${dataset} --model ${model} --lr ${lr} --setup_clients ${setup_clients} --num_actors ${actors} \
+               --batch-size ${batch_size} --num_gpus ${gpus} --seed ${seed} --clients-per-round ${per_round} \
+               --reg_weight ${lambda} --num-epochs ${epochs} --num-rounds ${num_rounds} --method ${method} -pc ${pc} -ps ${ps} \
+               --attack_type ${attack_type} --metrics-dir ../experiments/results/${dataset} --metrics_name \
+              ${output_name} > ../experiments/log/${dataset}/${output_name}.txt
+          done
     done
-
-for attack_type in 0 2 1
- do
-   for pc in 0.5 0.3
-     do
-       for seed in 0 1 2 3 4
-           do
-               output_name=${dataset}_${method}_${pc}_${ps}_${attack_type}_${lambda}_${seed}
-               python main.py --dataset ${dataset} --model ${model} --setup_clients ${setup_clients} -lr ${lr} --batch-size ${batch_size} --num_actors ${actors} --num_gpus ${gpus} --seed ${seed}  --clients-per-round ${per_round} --lamb ${lambda} --num-epochs ${epochs} --num-rounds ${num_rounds} --method ${method} -pc ${pc} -ps ${ps} --attack_type ${attack_type} --metrics-dir ../experiments/results/${dataset} --metrics_name \
-                 ${output_name} > ../experiments/log/${dataset}/${output_name}.txt
-           done
-     done
- done
+  done
 
 popd || exit
 popd || exit
