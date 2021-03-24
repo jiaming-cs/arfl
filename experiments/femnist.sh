84ec41bb1a526e284f099a6e18de63b808b126b8
Shenghui Li <shenghui.li@it.uu.se>
2021-03-24 22:12:41 +0100

    Update

--- experiments/femnist.sh
+++ experiments/femnist.sh
@@ -19,16 +19,16 @@ pushd ../
 pushd models/ || exit
 #method=$1
 for method in "arfl" "cfl" "mkrum" "rfa"
-for seed in 0 1 2 3 4
-    do
-        output_name=${dataset}_$1_${pc}_${ps}_${attack_type}_${lambda}_${seed}
-            python main.py --dataset ${dataset} --model cnn --setup_clients ${setup_clients} --num_actors ${actors} \
-               --batch-size ${batch_size} --num_gpus ${gpus} --seed ${seed} --clients-per-round ${per_round} \
-               --reg_weight ${lambda} --num-epochs ${epochs} --num-rounds ${num_rounds} --method ${method} -pc ${pc} -ps ${ps} \
-               --attack_type ${attack_type} --metrics-dir ../experiments/results/${dataset} --metrics_name \
-              ${output_name} > ../experiments/log/${dataset}/${output_name}.txt
-    done
-
+  for seed in 0 1 2 3 4
+      do
+          output_name=${dataset}_$1_${pc}_${ps}_${attack_type}_${lambda}_${seed}
+              python main.py --dataset ${dataset} --model cnn --setup_clients ${setup_clients} --num_actors ${actors} \
+                 --batch-size ${batch_size} --num_gpus ${gpus} --seed ${seed} --clients-per-round ${per_round} \
+                 --reg_weight ${lambda} --num-epochs ${epochs} --num-rounds ${num_rounds} --method ${method} -pc ${pc} -ps ${ps} \
+                 --attack_type ${attack_type} --metrics-dir ../experiments/results/${dataset} --metrics_name \
+                ${output_name} > ../experiments/log/${dataset}/${output_name}.txt
+      done
+done
 #for attack_type in 0 1 2
 #  do
 #    for pc in 0.3 0.5
